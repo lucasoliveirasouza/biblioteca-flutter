3c7c3ed23b0181e63c8b0878e5f993cb1841b15b
lucasoliveirasouza <>
2022-06-12 15:43:02 -0300

    Criar classe modelo de Livro #14

---
+++ lib/models/livro.dart
@@ -0,0 +1,58 @@
+class Categoria {
+  late int _id;
+  late String _titulo;
+  late String _autor;
+  late String _editora;
+  late int _ano;
+  late String _isbn;
+  late String _imagem;
+
+  Categoria? categoria;
+
+
+  Categoria(this._id, this._titulo, this._autor, this._editora, this._ano,
+      this._isbn, this._imagem);
+
+
+  String get imagem => _imagem;
+
+  set imagem(String value) {
+    _imagem = value;
+  }
+
+  String get isbn => _isbn;
+
+  set isbn(String value) {
+    _isbn = value;
+  }
+
+  int get ano => _ano;
+
+  set ano(int value) {
+    _ano = value;
+  }
+
+  String get editora => _editora;
+
+  set editora(String value) {
+    _editora = value;
+  }
+
+  String get autor => _autor;
+
+  set autor(String value) {
+    _autor = value;
+  }
+
+  String get titulo => _titulo;
+
+  set titulo(String value) {
+    _titulo = value;
+  }
+
+  int get id => _id;
+
+  set id(int value) {
+    _id = value;
+  }
+}
