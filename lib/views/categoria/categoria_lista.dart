2c82f090f9db3d69a94e881feb62b79dd7863720
lucasoliveirasouza <>
2022-06-12 14:03:09 -0300

    Listar categorias #6

--- lib/views/categoria/categoria_lista.dart
+++ lib/views/categoria/categoria_lista.dart
@@ -1,4 +1,7 @@
+import 'package:biblioteca/models/categoria.dart';
+import 'package:biblioteca/services/categoria_service.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class CategoriaListaView extends StatefulWidget {
   const CategoriaListaView({Key? key}) : super(key: key);
@@ -13,6 +16,24 @@ class _CategoriaListaViewState extends State<CategoriaListaView> {
     return Scaffold(
       appBar: AppBar(
         title: Text("Categorias"),
+      ),
+      body: Consumer<CategoriaService>(
+        builder: (context, repositorio, child) {
+          return ListView.separated(
+            itemCount: repositorio.categorias.length,
+            itemBuilder: (BuildContext contexto, int categoria) {
+              final List<Categoria> lista = repositorio.categorias;
+              return Card(
+                child: ListTile(
+                  title: Text(lista[categoria].descricao),
+
+                ),
+              );
+            },
+            separatorBuilder: (_, __) => Container(),
+            padding: EdgeInsets.all(16),
+          );
+        },
       ),
     );
   }
