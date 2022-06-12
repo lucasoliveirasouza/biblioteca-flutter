83f0e61460e4162a1186a95cfa24ecae4d26031f
lucasoliveirasouza <>
2022-06-12 13:55:33 -0300

    Criar método post de categoria #5

--- lib/services/categoria_service.dart
+++ lib/services/categoria_service.dart
@@ -1,6 +1,35 @@
+import 'dart:collection';
+import 'dart:convert';
 
+import 'package:biblioteca/models/categoria.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:http/http.dart' as http;
 
-class CategoriaService extends  ChangeNotifier{
+class CategoriaService extends ChangeNotifier {
+  List<Categoria> _categorias = [];
 
+  UnmodifiableListView<Categoria> get categorias =>
+      UnmodifiableListView(_categorias);
+
+  CategoriaService() {
+    _buscarCategorias();
+  }
+
+  _buscarCategorias() async {
+    String uri = 'https://biblioteca-lucas.herokuapp.com/api/categorias';
+    final response = await http.get(Uri.parse(uri), headers: {
+      'Content-Type': 'application/json'
+    });
+
+    if (response.statusCode == 200) {
+      final json = jsonDecode(response.body);
+      List<dynamic> listaCategorias = json;
+
+      listaCategorias.forEach((categoria) {
+        Categoria c = Categoria(categoria["id"], categoria["descricao"]);
+        _categorias.add(c);
+      });
+      notifyListeners();
+    }
+  }
 }
