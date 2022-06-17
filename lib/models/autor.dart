a402488840693852e6ab57807ba3b3ba83eeeda6
lucasoliveirasouza <>
2022-06-17 20:13:26 -0300

    Criar model de Autor #35

--- lib/models/autor.dart
+++ lib/models/autor.dart
@@ -1,19 +1,19 @@
-class Editora {
+class Autor{
   late String _id;
   late String _nome;
 
-  Editora(this._id, this._nome);
-
-  String get id => _id;
-
-  set id(String value) {
-    _id = value;
-  }
+  Autor(this._id, this._nome);
 
   String get nome => _nome;
 
   set nome(String value) {
     _nome = value;
+  }
+
+  String get id => _id;
+
+  set id(String value) {
+    _id = value;
   }
 
   Map<String, dynamic> toJson() => {'id': id, 'nome': _nome};
