1030ebdf5d36194d4b947543c056746e45c38944
lucasoliveirasouza <>
2022-06-17 20:40:11 -0300

    Criar service de Autor #37

---
+++ lib/services/autor_service.dart
@@ -0,0 +1,106 @@
+import 'dart:collection';
+import 'dart:convert';
+import 'package:biblioteca/models/autor.dart';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+class AutorService extends ChangeNotifier {
+  List<Autor> _autores = [];
+
+  UnmodifiableListView<Autor> get autores =>
+      UnmodifiableListView(_autores);
+
+  AutorService() {
+    _buscarAutores();
+  }
+
+  _buscarAutores() async {
+    String uri = 'https://biblioteca-lucas.herokuapp.com/api/autores';
+    final response = await http.get(Uri.parse(uri), headers: {
+      'Content-Type': 'application/json'
+    });
+
+    if (response.statusCode == 200) {
+      final json = jsonDecode(response.body);
+      List<dynamic> listaAutores = json;
+
+      listaAutores.forEach((autor) {
+        Autor at = Autor(autor["id"].toString(), autor["nome"]);
+        _autores.add(at);
+      });
+      notifyListeners();
+    }
+  }
+
+  Future<List<Autor?>?> getAll() async {
+    List<Autor> aut = [];
+    String uri = 'https://biblioteca-lucas.herokuapp.com/api/autores';
+    final response = await http.get(Uri.parse(uri), headers: {
+      'Content-Type': 'application/json'
+    });
+
+    if (response.statusCode == 200) {
+      final json = jsonDecode(response.body);
+      List<dynamic> listaautores = json;
+
+      listaautores.forEach((autor) {
+        Autor at = Autor(autor["id"].toString(), autor["nome"]);
+        aut.add(at);
+      });
+
+    }
+    return autores;
+  }
+
+  Future<http.Response> cadastrarAutor(String nome) async{
+    final http.Response response = await http.post(
+      Uri.parse('https://biblioteca-lucas.herokuapp.com/api/autor'),
+      headers: <String, String>{
+        'Content-Type': 'application/json',
+      },
+      body: jsonEncode(<String, String>{
+        'nome': nome,
+      }),
+    );
+
+    _autores.clear();
+    _buscarAutores();
+    notifyListeners();
+
+    return response;
+  }
+
+  Future<http.Response> deletarAutor(String id) async {
+    final http.Response response = await http.delete(
+      Uri.parse('https://biblioteca-lucas.herokuapp.com/api/autor/${id}'),
+      headers: <String, String>{
+        'Content-Type': 'application/json',
+      },
+    );
+    _autores.clear();
+    _buscarAutores();
+    notifyListeners();
+    return response;
+  }
+
+  Future<http.Response> editarAutor(String id, String nome) async{
+    final http.Response response = await http.put(
+      Uri.parse('https://biblioteca-lucas.herokuapp.com/api/autor'),
+      headers: <String, String>{
+        'Content-Type': 'application/json',
+      },
+      body: jsonEncode(<String, String>{
+        'id': id,
+        'nome': nome,
+      }),
+    );
+
+    _autores.clear();
+    _buscarAutores();
+    notifyListeners();
+
+    return response;
+  }
+
+
+}
