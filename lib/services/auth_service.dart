687c66e43e5f393b8acc34ebb7236fc3835aa94d
lucasoliveirasouza <>
2022-06-19 19:06:01 -0300

    Criar método de logout #80

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -4,45 +4,44 @@ import 'package:flutter/material.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:get/get.dart';
 import 'package:http/http.dart' as http;
-import 'package:provider/provider.dart';
-import 'package:biblioteca/services/categoria_service.dart';
+
 
 class AuthService extends ChangeNotifier{
 
   String _token = "";
+
   AuthService() {
-    _authCheck();
-  }
-
-  _authCheck() {
-
-    _token = "";
   }
 
   Future<String> logar(String usuario, String senha) async{
-    final http.Response response = await http.post(
-      Uri.parse("https://biblioteca-luc.herokuapp.com/api/auth/signin"),
-      headers: <String, String>{
-        'Content-Type': 'application/json',
-      },
-      body: jsonEncode(<String, String>{
-        "password": senha,
-        "username": usuario
-      }),
-    );
-    if (response.statusCode == 200) {
-      _token = jsonDecode(response.body)["accessToken"];
-      final storage = new FlutterSecureStorage();
-      await storage.write(key: "token", value: _token);
-      Get.to(() => MenuView());
-      return "Seja bem-vindo";
-    }else{
-      return "Existe algum erro com suas credenciais";
-    }
-  }
+    final storage = new FlutterSecureStorage();
+    String? value = await storage.read(key: "token");
+    if(value == null){
+      final http.Response response = await http.post(
+        Uri.parse("https://biblioteca-luc.herokuapp.com/api/auth/signin"),
+        headers: <String, String>{
+          'Content-Type': 'application/json',
+        },
+        body: jsonEncode(<String, String>{
+          "password": senha,
+          "username": usuario
+        }),
+      );
+      if (response.statusCode == 200) {
+        _token = jsonDecode(response.body)["accessToken"];
 
-  String getToken(){
-    return _token;
+        await storage.write(key: "token", value: _token);
+        Get.to(() => MenuView());
+        return "Seja bem-vindo";
+      }else{
+        return "Existe algum erro com suas credenciais";
+      }
+
+    }else{
+      Get.to(() => MenuView());
+    }
+    return "Logado";
+
   }
 
   Future<String> registrar(String usuario,String email, String senha) async {
@@ -58,6 +57,11 @@ class AuthService extends ChangeNotifier{
       }),
     );
     return jsonDecode(response.body)["message"] ?? "Não foi possível realizar o cadastro";
+  }
+
+  logout() async{
+    final storage = new FlutterSecureStorage();
+    await storage.delete(key: "token");
   }
 
 
