4c48e30bf6ebaa8f3d0882405bd5cfc8e61bc19a
lucasoliveirasouza <>
2022-06-19 18:43:49 -0300

    Adiconar Secure Storage em Livro #78

--- lib/services/livro_service.dart
+++ lib/services/livro_service.dart
@@ -6,10 +6,12 @@ import 'package:biblioteca/models/editora.dart';
 import 'package:biblioteca/models/livro.dart';
 import 'package:biblioteca/util/constantes.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:http/http.dart' as http;
 
 class LivroService extends ChangeNotifier {
   List<Livro> _livros = [];
+  final storage = new FlutterSecureStorage();
 
   UnmodifiableListView<Livro> get livros => UnmodifiableListView(_livros);
 
@@ -18,24 +20,31 @@ class LivroService extends ChangeNotifier {
   }
 
   _buscarLivros() async {
+    String? value = await storage.read(key: "token");
     String uri = '${servidor1}api/livros';
-    final response = await http
-        .get(Uri.parse(uri), headers: {'Content-Type': 'application/json'});
+    final response = await http.get(Uri.parse(uri), headers: {
+      'Content-Type': 'application/json',
+      'Authorization': "Bearer ${value}"
+    });
 
     if (response.statusCode == 200) {
       final json = jsonDecode(response.body);
       List<dynamic> listaLivros = json;
 
       listaLivros.forEach((livro) {
-        Livro l = Livro(livro["id"], livro["titulo"], livro["ano"], livro["isbn"], livro["imagem"]);
+        Livro l = Livro(livro["id"], livro["titulo"], livro["ano"],
+            livro["isbn"], livro["imagem"]);
 
-        Categoria c = Categoria(livro["categoria"]["id"].toString(), livro["categoria"]["descricao"]);
+        Categoria c = Categoria(livro["categoria"]["id"].toString(),
+            livro["categoria"]["descricao"]);
         l.setCategoria(c);
 
-        Editora e = Editora(livro["editora"]["id"].toString(), livro["editora"]["nome"]);
+        Editora e = Editora(
+            livro["editora"]["id"].toString(), livro["editora"]["nome"]);
         l.setEditora(e);
 
-        Autor a = Autor(livro["autor"]["id"].toString(), livro["autor"]["nome"]);
+        Autor a =
+            Autor(livro["autor"]["id"].toString(), livro["autor"]["nome"]);
         l.setAutor(a);
 
         _livros.add(l);
@@ -44,11 +53,15 @@ class LivroService extends ChangeNotifier {
     }
   }
 
-  Future<String> cadastrarLivro(Livro livro, String id_categoria,String id_autor, String id_editora) async{
+  Future<String> cadastrarLivro(Livro livro, String id_categoria,
+      String id_autor, String id_editora) async {
+    String? value = await storage.read(key: "token");
     final http.Response response = await http.post(
-      Uri.parse("${servidor1}api/livro/${id_categoria}/${id_autor}/${id_editora}"),
+      Uri.parse(
+          "${servidor1}api/livro/${id_categoria}/${id_autor}/${id_editora}"),
       headers: <String, String>{
         'Content-Type': 'application/json',
+        'Authorization': "Bearer ${value}"
       },
       body: jsonEncode(<String, String>{
         "ano": livro.ano.toString(),
@@ -64,12 +77,15 @@ class LivroService extends ChangeNotifier {
 
       print(livro["titulo"]);
 
-      Livro lv = Livro(livro["id"], livro["titulo"], livro["ano"], livro["isbn"], livro["imagem"]);
+      Livro lv = Livro(livro["id"], livro["titulo"], livro["ano"],
+          livro["isbn"], livro["imagem"]);
 
-      Categoria c = Categoria(livro["categoria"]["id"].toString(), livro["categoria"]["descricao"]);
+      Categoria c = Categoria(
+          livro["categoria"]["id"].toString(), livro["categoria"]["descricao"]);
       lv.setCategoria(c);
 
-      Editora e = Editora(livro["editora"]["id"].toString(), livro["editora"]["nome"]);
+      Editora e =
+          Editora(livro["editora"]["id"].toString(), livro["editora"]["nome"]);
       lv.setEditora(e);
 
       Autor a = Autor(livro["autor"]["id"].toString(), livro["autor"]["nome"]);
@@ -79,16 +95,20 @@ class LivroService extends ChangeNotifier {
       notifyListeners();
 
       return "Cadatrado com sucesso";
-    }else{
+    } else {
       return "Não foi possível realizar o cadastro";
     }
   }
 
-  Future<String> editarLivro(Livro livro, String id_categoria,String id_autor,String id_editora) async{
+  Future<String> editarLivro(Livro livro, String id_categoria, String id_autor,
+      String id_editora) async {
+    String? value = await storage.read(key: "token");
     final http.Response response = await http.put(
-      Uri.parse("${servidor1}api/livro/${id_categoria}/${id_autor}/${id_editora}"),
+      Uri.parse(
+          "${servidor1}api/livro/${id_categoria}/${id_autor}/${id_editora}"),
       headers: <String, String>{
         'Content-Type': 'application/json',
+        'Authorization': "Bearer ${value}"
       },
       body: jsonEncode(<String, String>{
         'id': livro.id.toString(),
@@ -120,17 +140,20 @@ class LivroService extends ChangeNotifier {
         }
       });
       return "Editado com sucesso";
-    }else{
+    } else {
       return "Não foi possível editar";
     }
   }
 
   Future<http.Response> deletarLivro(String id_livro) async {
+    String? value = await storage.read(key: "token");
     print("${servidor1}api/livro/${id_livro}/");
     final http.Response response = await http.delete(
-      Uri.parse("https://biblioteca-lucas.herokuapp.com/api/livro/${id_livro}/"),
+      Uri.parse(
+          "https://biblioteca-lucas.herokuapp.com/api/livro/${id_livro}/"),
       headers: <String, String>{
         'Content-Type': 'application/json',
+        'Authorization': "Bearer ${value}"
       },
     );
 
